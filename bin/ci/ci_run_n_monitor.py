87c5841ad60a9ff8a11f31688cfe02054875886b
Guilherme Gallo <guilherme.gallo@collabora.com>
2025-02-26 01:31:06 -0300

    bin/ci: crnm: Avoid clamping the job name
    Right now, the padding calculation only considers dependency jobs, but
    some tables that use it contains unrelated jobs, for example the manual
    ones that are enabled (created -> manual) when we play the first jobs in
    the chain.
    
    So let's slack a bit the padding when that happens.
    
    Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>

--- bin/ci/ci_run_n_monitor.py
+++ bin/ci/ci_run_n_monitor.py
@@ -633,7 +633,7 @@ def __job_duration_record(dict_item: tuple) -> str:
 
 
 def link2print(url: str, text: str, text_pad: int = 0) -> str:
-    text_pad = len(text) if text_pad < 1 else text_pad
+    text_pad = max(text_pad, len(text)) + len(URL_END)
     return f"{URL_START}{url}\a{text:{text_pad}}{URL_END}"
 
 
