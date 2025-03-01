3c33a7e360e3206a5908da324434a913d4270c32
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2025-03-01 18:14:38 -0500

    radv: use nir_lower_sparse_resident_query
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/amd/vulkan/nir/radv_nir_lower_intrinsics_early.c
+++ src/amd/vulkan/nir/radv_nir_lower_intrinsics_early.c
@@ -13,34 +13,26 @@
 static bool
 pass(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 {
-   bool *lower_view_index_to_zero = data;
-   nir_def *def = NULL;
-   b->cursor = nir_before_instr(&intrin->instr);
-
-   switch (intrin->intrinsic) {
-   case nir_intrinsic_is_sparse_texels_resident:
-      def = nir_ieq_imm(b, intrin->src[0].ssa, 0);
-      break;
-   case nir_intrinsic_sparse_residency_code_and:
-      def = nir_ior(b, intrin->src[0].ssa, intrin->src[1].ssa);
-      break;
-   case nir_intrinsic_load_view_index:
-      if (!(*lower_view_index_to_zero))
-         return false;
-
-      def = nir_imm_zero(b, 1, 32);
-      break;
-   default:
+   if (intrin->intrinsic != nir_intrinsic_load_view_index)
       return false;
-   }
 
-   nir_def_replace(&intrin->def, def);
+   b->cursor = nir_before_instr(&intrin->instr);
+   nir_def_replace(&intrin->def, nir_imm_zero(b, 1, 32));
    return true;
 }
 
 bool
 radv_nir_lower_intrinsics_early(nir_shader *nir, bool lower_view_index_to_zero)
 {
-   return nir_shader_intrinsics_pass(nir, pass, nir_metadata_control_flow,
-                                     &lower_view_index_to_zero);
+   bool progress = false;
+
+   NIR_PASS(progress, nir, nir_lower_sparse_resident_query,
+            NIR_SPARSE_BIT_ALL | NIR_SPARSE_BIT_INVERTED);
+
+   if (lower_view_index_to_zero) {
+      NIR_PASS(progress, nir, nir_shader_intrinsics_pass, pass,
+               nir_metadata_control_flow, NULL);
+   }
+
+   return progress;
 }
